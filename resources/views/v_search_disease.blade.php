7f1a5228ca79cee5bb8a9191f45f54a64b7a3dff
Worasak <58160497@go.buu.ac.th>
2019-02-18 07:28:08 +0700

    add file disease

---
+++ resources/views/v_search_disease.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Search disease</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+
+            /* Class Manage message display */
+
+            .Fontt {
+                font-family: 'Prompt', sans-serif;
+                color: #000000;
+                font-size: 20px;
+            }
+
+            .FontBody {
+                font-family: 'Prompt', sans-serif;
+            }
+            /* Class btn in images display */
+            .text-image {
+                position: relative;
+                text-align: center;
+                color: blue;
+
+              }
+              .centered-articleForHealth {
+                position: absolute;
+                top: 18%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+              }
+
+
+              .centered {
+                position: absolute;
+                top: 40%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+              }
+              .btn-image
+              {
+                position: absolute;
+                top: 60%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                border-radius: 12px;
+                margin: 4px 2px;
+              }
+
+              #icon-forHelath {
+
+                background: white url(Images_inView/magnifying-glass.png) no-repeat;
+                position: relative;
+                background-size: 3%;
+                background-position-x: 97%;
+                background-position-y: 50%;
+                width: 700px;
+            }
+            #icon{
+
+                background: white url(Images_inView/magnifying-glass.png) no-repeat;
+                position: relative;
+                background-size: 3%;
+                background-position-x: 97%;
+                background-position-y: 50%;
+
+            }
+
+        </style>
+
+
+
+
+    </head>
+
+ <body>
+</body>
+</html>
